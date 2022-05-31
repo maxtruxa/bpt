b0820f463bf7f63cc73cdbce6613e14d1806ddef
Max Truxa <dev@maxtruxa.com>
2022-05-31 09:52:55 +0000

    Actually create temporary directories on *nix
    `mktemp` doesn't actually create the directory and is pretty broken on some
    platforms. `mkdtemp`, `mkstemp` and friends are here to fix this.

--- src/bpt/temp.nix.cpp
+++ src/bpt/temp.nix.cpp
@@ -1,12 +1,28 @@
 #include "./temp.hpp"
 
 #ifndef _WIN32
+
+#include <vector>
+
 using namespace bpt;
+
+namespace {
+
+auto string_to_vector(std::string const& s)
+{
+    std::vector<char> v(s.size() + 1);
+    std::copy_n(s.c_str(), s.size() + 1, v.begin());
+    return v;
+}
+
+} // namespace
 
 temporary_dir temporary_dir::create_in(path_ref base) {
     auto file = (base / "bpt-tmp-XXXXXX").string();
 
-    const char* tempdir_path = ::mktemp(file.data());
+    // mkdtemp() modifies the input buffer.
+    auto templ = string_to_vector(file);
+    const char* tempdir_path = ::mkdtemp(templ.data());
 
     if (tempdir_path == nullptr) {
         throw std::system_error(std::error_code(errno, std::system_category()),
@@ -15,4 +31,5 @@ temporary_dir temporary_dir::create_in(path_ref base) {
     auto path = fs::path(tempdir_path);
     return std::make_shared<impl>(std::move(path));
 }
+
 #endif
